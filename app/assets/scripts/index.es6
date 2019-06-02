4c5260478854865a34981628cafb061cbcdd41f5
SilvesterChiao <SilvesterChiao@gmail.com>
2019-06-02 23:43:42 +0800

    '修改webpack配置'

---
+++ app/assets/scripts/index.es6
@@ -0,0 +1,17 @@
+import Mustache from 'mustache'
+import { sex, echo } from './object.es6'
+
+function component() {
+    var element = document.createElement('div')
+
+    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ')
+
+    return element
+}
+
+// document.body.appendChild(component());
+
+echo(sex)
+
+console.log(Mustache)
--- webpack.config.js
+++ webpack.config.js
@@ -1,84 +1,94 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CleanWebpackPlugin = require('clean-webpack-plugin')
+// webpack
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 function resolve(dir) {
-    return path.join(__dirname, '..', dir)
+  return path.join(__dirname, '..', dir);
 }
 
 module.exports = {
-    entry: __dirname + '/app/main.js', //已多次提及的唯一入口文件
-    output: {
-        path: __dirname + '/build', //打包后的文件存放的地方
-        filename: 'bundle-[hash].js' //打包后输出文件的文件名
-    },
-    devtool: 'eval-source-map',
-    devServer: {
-        contentBase: './build',
-        port: 3004,
-        historyApiFallback: true,
-        inline: true
-    },
-    module: {
-        rules: [
-            {
-                test: /(\.jsx|\.js)$/,
-                use: {
-                    loader: 'babel-loader'
-                },
-                exclude: /node_modules/
+  // 打包唯一入口，已多次提及的唯一入口文件
+  entry: __dirname + '/app/main.js',
+  output: {
+    // 打包输入目录
+    path: __dirname + '/build',
+    // 打包文件名
+    filename: 'bundle-[hash].js',
+  },
+  // devtool: 'source-map',
+  // devtool: 'cheap-module-source-map',
+  // devtool: 'cheap-module-eval-source-map',
+  // 仅限开发环境
+  devtool: 'eval-source-map',
+  // 本地服务器 webpack-dev-server
+  devServer: {
+    contentBase: './build',
+    port: 3004,
+    historyApiFallback: true,
+    inline: true,
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[name]__[local]--[hash:base64:5]',
             },
-            {
-                test: /\.css$/,
-                use: [
-                    {
-                        loader: 'style-loader'
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            localIdentName: '[name]__[local]--[hash:base64:5]'
-                        }
-                    },
-                    {
-                        loader: 'postcss-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 10000,
-                    // name: utils.assetsPath('img/[name].[hash:7].[ext]'),
-                    name: 'img/[name].[hash:7].[ext]'
-                }
-            }
-        ]
-    },
-    plugins: [
-        new webpack.BannerPlugin('版权所有，翻版必究'),
-        new HtmlWebpackPlugin({
-            template: __dirname + '/app/index.tmpl.html'
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new CleanWebpackPlugin('build/*.*', {
-            root: __dirname,
-            verbose: true,
-            dry: false
-        })
+          },
+          {
+            loader: 'postcss-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          // name: utils.assetsPath('img/[name].[hash:7].[ext]'),
+          name: 'img/[name].[hash:7].[ext]',
+        },
+      },
     ],
-    resolve: {
-        extensions: ['.css', '.js', '.vue', '.scss', '.json'],
-        alias: {
-            '@': resolve('app'),
-            images: path.resolve(__dirname, 'app/assets/images'),
-            styles: path.resolve(__dirname, 'app/assets/styles/')
-        }
+  },
+  plugins: [
+    new webpack.BannerPlugin('版权所有，翻版必究'),
+    new HtmlWebpackPlugin({
+      template: __dirname + '/app/index.tmpl.html',
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CleanWebpackPlugin('build/*.*', {
+      root: __dirname,
+      verbose: true,
+      dry: false,
+    }),
+  ],
+  resolve: {
+    extensions: ['.css', '.js', '.vue', '.scss', '.json'],
+    alias: {
+      '@': resolve('app'),
+      images: path.resolve(__dirname, 'app/assets/images'),
+      styles: path.resolve(__dirname, 'app/assets/styles/'),
     },
-    externals: {
-        jquery: 'jQuery'
-    }
-}
+  },
+  externals: {
+    jquery: 'jQuery',
+  },
+};
